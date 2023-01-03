f03d42f7a698bd05e64c00756f3dbe7c4312dd28
BadAga <agnieszka.badocha@gmail.com>
2023-01-03 18:56:07 +0100

    feat: jwt

---
+++ TravelRecordsAPI/TravelRecordsAPI/Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TravelRecordsAPI.Models;
+using TravelRecordsAPI.Models.Dto;
+
+namespace TravelRecordsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private readonly CoreDbContext _context;
+
+        public LoginController(IConfiguration config, CoreDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserDto userLogin)
+        {
+            var user = _context.Users.FirstOrDefault(e => e.Username == userLogin.Username &&
+                                                            e.Password == userLogin.Password);
+            if(user!=null)
+            {
+                var token = GenerateToken(user);
+                return Ok(token);
+            }
+
+            user = _context.Users.FirstOrDefault(e => e.Username == userLogin.Username);
+
+            if(user!=null)
+            {
+                return NotFound("Wrong password");
+            }
+            return NotFound("User not found");
+        }
+
+        private String GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials=new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] 
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Username),
+                new Claim(ClaimTypes.Sid,user.UserId.ToString()),
+                new Claim(ClaimTypes.Role,"User")
+            };
+
+            var token=new JwtSecurityToken(claims:claims, 
+                                            expires:DateTime.Now.AddMinutes(15),
+                                            signingCredentials:credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- TravelRecordsAPI/TravelRecordsAPI/Program.cs
+++ TravelRecordsAPI/TravelRecordsAPI/Program.cs
@@ -2,15 +2,35 @@ using Microsoft.EntityFrameworkCore;
 using TravelRecordsAPI.Models;
 using TravelRecordsAPI.Repository;
 using TravelRecordsAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddMvc();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+       
+    };
+});
 
 //connect to DB
 var connectionString = builder.Configuration.GetConnectionString("TRConnection");
@@ -31,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
