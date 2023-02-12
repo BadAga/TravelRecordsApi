cea071de7c9689fbdf491eae2aa7d17e7d78079d
BadAga <agnieszka.badocha@gmail.com>
2023-02-12 19:35:22 +0100

    feat: latitude and longitude

--- TravelRecordsAPI/TravelRecordsAPI/Models/Stage.cs
+++ TravelRecordsAPI/TravelRecordsAPI/Models/Stage.cs
@@ -11,15 +11,17 @@ namespace TravelRecordsAPI.Models
     {
         [Key]
         public int StageId { get; set; }
-        public int UserId { get; set; }
-        public int TripId { get; set; }
+        public int? UserId { get; set; }
+        public int? TripId { get; set; }
         [StringLength(50)]
         [Unicode(false)]
         public string Title { get; set; } = null!;
-        [StringLength(150)]
+        [StringLength(200)]
         [Unicode(false)]
         public string StageDesc { get; set; } = null!;
         [Column(TypeName = "date")]
         public DateTime? CreationDate { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
     }
 }
